76b2c1d5c12f36b930ef4353c00948789e697a4d
Trevor Biri <tmbiri91@gmail.com>
2025-02-21 16:50:16 -0600

    create class and functions to parse data

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,22 @@
 import { getQuiz, quizBook } from '../data/data.js';
 
+export class Question {
+  constructor(question) {
+    this.question = question.question;
+    this.cAnswer = question.correct_answer;
+    this.iAnswers = question.incorrect_answers;
+    this.answers = this.#calcAllAnswers([...this.iAnswers], this.cAnswer);
+    this.difficulty = question.difficulty;
+    this.category = question.category;
+  }
+
+  #calcAllAnswers(iAnswers, cAnswer) {
+    const randomIndex = Math.floor(Math.random() * (iAnswers.length + 1));
+    iAnswers.splice(randomIndex, 0, cAnswer);
+    return iAnswers;
+  }
+}
+
 let overlay = document.querySelector('.menu-overlay');
 let header2 = document.getElementById('header2');
 let menu = document.querySelector('.header-icon');
@@ -28,12 +45,16 @@ function openQuiz(event) {
     console.log(category, difficulty);
     getQuiz(category, difficulty).then(() =>
       quizBook.results.forEach((question) => {
-        console.log(question.difficulty);
-        console.log(question.question);
+        buildQuestions(question);
       })
     );
   }
 }
 
+function buildQuestions(question) {
+  let quizQuestion = new Question(question);
+  console.log(quizQuestion);
+  console.log(quizQuestion.answers);
+}
 document.addEventListener('click', menuControl);
 header2.addEventListener('click', openQuiz);
